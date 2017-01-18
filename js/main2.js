5068d594150665b8ba4e5dd50cdd3c33f1296b0d
K6NO <tamas.kenessey@gmail.com>
2017-01-18 19:33:01 +0100

    working js and jQuery with minor issue
    Nr of pagination links doesn’t change dynamically

--- js/main2.js
+++ js/main2.js
@@ -10,6 +10,10 @@ var allStudents = [];
 //var searchedStudent = [];
 
 
+//create No results message
+var noStudentsMessage = $('<p class="no-result">No results for your search.</p>').insertAfter($('.student-list'));
+
+
 // save all students to a list
 $('.student-item').children('.student-details').each(function () {
     allStudents.push(this);
@@ -19,6 +23,11 @@ $('.student-item').children('.student-details').each(function () {
 // hide all students, show the first ten
 
 $('.student-item').hide().slice(0, 10).show();
+
+// hide No Results message
+
+noStudentsMessage.hide();
+
 
 // create pagination links
 var paginationContainer = $('<div class="pagination"><ul></ul></div>');
@@ -44,6 +53,7 @@ $('.pagination li').on('click', function() {
         $(this).index() * 10,
         $(this).index() * 10 + 10)
         .show();
+    noStudentsMessage.hide();
 })
 
 //add search functionality
@@ -53,39 +63,48 @@ $('.page-header').append(searchElement);
 // when search button is clicked...
 $('.student-search button').on('click', function() {
 
+    noStudentsMessage.hide();
+
     var searchedStudent = [];
 
     var searchTerm = $('.student-search input').val();
     console.log("searchTerm " + searchTerm);
 
+
+    for (var i = 0; i < allStudents.length; i++) {
+        var studentName = allStudents[i].innerText;
+        //console.log(studentName);
+
+        if (studentName.indexOf(searchTerm) !== -1) {
+
+            console.log("Student found.");
+
+            //hide all students
+            $('.student-list li').css('display', 'none');
+
+            // add students to the show list, display them
+            searchedStudent.push(allStudents[i]);
+
+            // clear input field
+            $('.student-search input').val('');
+        }
+    }
+
+    $(searchedStudent).parent().show();
+
+    if (searchedStudent.length === 0) {
+        //hide all students
+        $('.student-list li').css('display', 'none');
+        console.log("No results");
+        $('.student-search input').val('');
+
+        noStudentsMessage.show();
+    }
+
     if (searchTerm === '') {
         $('.student-item').hide().slice(0, 10).show();
-    } else {
-        for (var i = 0; i < allStudents.length; i++) {
-            var studentName = allStudents[i].innerText;
+        noStudentsMessage.hide();
 
-            if (allStudents.indexOf(searchTerm) === -1) {
-
-                //hide all students
-                $('.student-list li').css('display', 'none');
-
-                $('<p>No results for your search.</p>').insertAfter($('.student-list'));
-                $('.student-search input').val('');
-                break;
-            } else {
-                console.log("Inside if.");
-
-                //hide all students
-                $('.student-list li').css('display', 'none');
-
-                // add students to the show list, display them
-                searchedStudent.push(allStudents[i]);
-                console.log(searchedStudent);
-
-                $(searchedStudent).css('display', 'block');
-                $('.student-search input').val('');
-            }
-        }
     }
 })
 
